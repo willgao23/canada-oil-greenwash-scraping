f0e74f88b11f7d00a510212ac104826409d315a4
wgao23 <wgao23@students.cs.ubc.ca>
2025-10-27 15:19:44 -0700

    Implement Enbridge fetcher

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 WAYBACK_ENDPOINT = "http://web.archive.org/cdx/search/cdx"
 WAYBACK_PREFIX = "http://web.archive.org/web"
-BILL_C59_ROYAL_ASSENT_DATE = 20240620
+BILL_C59_ROYAL_ASSENT_DATE = 20240620000000
 URLS = {
     "Suncor Energy": {
         "current": "https://www.suncor.com/en-ca/news-and-stories/news-releases"
@@ -11,5 +11,6 @@ URLS = {
     "Imperial Oil": {
         "current": "https://news.imperialoil.ca/news-releases/default.aspx"
     },
+    "Enbridge": {"current": "https://www.enbridge.com/media-center/news"},
 }
 LINK_CSV_FIELDS = ["Organization", "Link", "Date Scraped", "Type"]
