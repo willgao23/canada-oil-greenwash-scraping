7998d5f12231214564afa6864be7851a20e42667
wgao23 <wgao23@students.cs.ubc.ca>
2026-01-12 14:22:07 -0800

    Update fetcher to pull unhosted links

--- fetcher.py
+++ fetcher.py
@@ -4,6 +4,7 @@ from config import (
     BILL_C59_ROYAL_ASSENT_DATE,
     LINK_CSV_FIELDS,
     WAYBACK_PREFIX,
+    WAYBACK_ORGS,
 )
 import requests
 from selenium import webdriver
@@ -15,6 +16,9 @@ from datetime import datetime
 import time
 import os
 import random
+import pandas as pd
+from tqdm import tqdm
+import os, os.path
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(5)
@@ -27,14 +31,14 @@ session.headers.update(
 )
 
 
-def fetch_wayback_url(url, max_retries=5):
+def fetch_wayback_url(url, max_retries=5, to=None, limit=-1):
     for attempt in range(max_retries):
         try:
             payload = {
                 "url": url,
                 "output": "json",
-                "to": BILL_C59_ROYAL_ASSENT_DATE,
-                "limit": -1,
+                "to": to,
+                "limit": limit,
             }
             res = session.get(WAYBACK_ENDPOINT, params=payload, timeout=25)
             res.raise_for_status()
@@ -191,7 +195,10 @@ def fetch_enbridge_article_urls(url, is_archive, is_root):
         year_urls = [year_tab.get_attribute("href") for year_tab in year_tabs]
         year_urls = year_urls[1:]
         if is_archive:
-            year_urls = [fetch_wayback_url(year_url) for year_url in year_urls]
+            year_urls = [
+                fetch_wayback_url(year_url, to=BILL_C59_ROYAL_ASSENT_DATE)
+                for year_url in year_urls
+            ]
         new_rows.extend(
             row
             for year_url in year_urls
@@ -320,3 +327,110 @@ def fetch_urls():
 
     print("Fetched all article URLs!")
     driver.quit()
+
+
+def download_pdfs(links, is_archive):
+    pdf_links_df = links[links["Type"] == "pdf"]
+    pdf_orgs = list(pdf_links_df["Organization"].unique())
+    for org in pdf_orgs:
+        dir_path = (
+            f"output\pdfs\{org}\\archived"
+            if is_archive
+            else f"output\pdfs\{org}\current"
+        )
+        download_dir = os.path.abspath(dir_path)
+        os.makedirs(download_dir, exist_ok=True)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option(
+            "prefs",
+            {
+                "download.default_directory": download_dir,
+                "download.prompt_for_download": False,
+                "plugins.always_open_pdf_externally": True,
+                "profile.default_content_setting_values.automatic_downloads": 1,
+                "safebrowsing.enabled": False,
+                "safebrowsing.disable_extension_blacklist": True,
+            },
+        )
+        chrome_options.add_argument("--allow-running-insecure-content")
+        chrome_options.add_argument("--ignore-certificate-errors")
+        chrome_options.add_argument("--disable-web-security")
+        chrome_options.add_argument(
+            "--unsafely-treat-insecure-origin-as-secure=http://archive.org,http://wayback.com"
+        )
+        driver = webdriver.Chrome(options=chrome_options)
+        pdf_links = pdf_links_df[pdf_links_df["Organization"] == org]["Link"].to_list()
+
+        start = (
+            len(
+                [
+                    entry
+                    for entry in os.listdir(f".\{dir_path}")
+                    if os.path.isfile(os.path.join(dir_path, entry))
+                ]
+            )
+            - 1
+        )
+        for pdf_link in tqdm(pdf_links[start:]):
+            if is_archive:
+                try:
+                    driver.get(pdf_link)
+                    iframe = driver.find_element(By.ID, "playback")
+                    driver.switch_to.frame(iframe)
+                    save_btn = driver.find_element(By.ID, "open-button")
+                    save_btn.click()
+                    time.sleep(2)
+                except:
+                    print(f"Error downloading pdf: {pdf_link}")
+                finally:
+                    driver.switch_to.default_content()
+            else:
+                driver.get(pdf_link)
+                time.sleep(1)
+        driver.quit()
+
+
+def fetch_unhosted_wayback_links(archived_links):
+    file_path = f"output/links/unhosted_wayback_links.csv"
+    with open(file_path, "w", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Wayback Link", "Link"])
+
+    for org in WAYBACK_ORGS:
+        links = archived_links[archived_links["Organization"] == org]["Link"].to_list()
+        for link in tqdm(links):
+            wayback_link = fetch_wayback_url(link, limit=1)
+            try:
+                with open(file_path, "a", newline="", encoding="utf-8") as f:
+                    writer = csv.writer(f)
+                    if link:
+                        writer.writerow([wayback_link, link])
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+
+def merge_unhosted_wayback():
+    unhosted_df = pd.read_csv("output/links/unhosted_wayback_links.csv")
+    archived_links = pd.read_csv("output/links/wayback_article_links.csv")
+    merged_df = pd.merge(
+        archived_links,
+        unhosted_df,
+        on="Link",
+        how="left",
+        suffixes=("", "_new"),
+    )
+    merged_df["Link"] = merged_df["Wayback Link"].fillna(merged_df["Link"])
+    final_df = merged_df.drop(columns=["Wayback Link"])
+    final_df.to_csv("output/links/merged_wayback_article_links.csv", index=False)
+
+
+def fetch_pdfs():
+    curr_links = pd.read_csv("output/links/article_links.csv")
+    archived_links = pd.read_csv("output/links/wayback_article_links.csv")
+    # fetch_unhosted_wayback_links(archived_links)
+    # merge_unhosted_wayback()
+    updated_archived_links = pd.read_csv(
+        "output/links/merged_wayback_article_links.csv"
+    )
+    # download_pdfs(curr_links, False)
+    download_pdfs(updated_archived_links, True)
