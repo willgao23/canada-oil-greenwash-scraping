293a76c1e5d836b4da0332d7051564be68b0373f
wgao23 <wgao23@students.cs.ubc.ca>
2026-03-29 00:09:15 -0700

    Add rare class labelling

---
+++ src/labelling/label_cli.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+import os
+import csv
+from src.config import LABEL_CSV_FIELDS, NUM_TRAIN
+
+
+def prompt_user_labelling(to_label):
+    with open(
+        "../../output/labelled/labelled.csv", "a", newline="", encoding="utf-8"
+    ) as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=LABEL_CSV_FIELDS)
+        if os.stat("../../output/labelled/labelled.csv").st_size == 0:
+            writer.writeheader()
+        for _, row in to_label.iterrows():
+            label = input(
+                f"Label this sentence as 0 (not a green claim) / 1 (green claim) (or q to quit):\n{row['Sentence']}\n> "
+            ).strip()
+
+            if label.lower() == "q":
+                print("Quitting...")
+                break
+
+            if label not in ["0", "1"]:
+                print("Invalid input. Please enter 0 or 1.")
+                continue
+
+            label = int(label)
+            print(f"You labeled it as: {label}")
+            row["Label"] = label
+            writer.writerow(row.to_dict())
+    return
+
+
+def label_data():
+    df = pd.read_csv("../../output/labelled/labelled.csv")
+    if not os.path.exists("../../output/labelled/labelled.csv") or df.empty:
+        sentences_df = pd.read_csv("../../output/processed/sentences.csv")
+        wayback_sentences_df = pd.read_csv(
+            "../../output/processed/wayback_sentences.csv"
+        )
+        sentences_df["isWayback"] = False
+        wayback_sentences_df["isWayback"] = True
+        combined_df = pd.concat([sentences_df, wayback_sentences_df], ignore_index=True)
+        combined_df["Label"] = -1
+        combined_df = combined_df.sample(frac=1, random_state=42).reset_index(drop=True)
+        prompt_user_labelling(combined_df.iloc[:NUM_TRAIN])
+        combined_df.iloc[NUM_TRAIN:].to_csv(
+            "../../output/labelled/unlabelled.csv",
+            index=False,
+            sep=",",
+            encoding="utf-8",
+        )
+    else:
+        combined_df = pd.read_csv("../../output/labelled/unlabelled.csv")
+        prompt_user_labelling(combined_df.iloc[:NUM_TRAIN])
+        combined_df.iloc[NUM_TRAIN:].to_csv(
+            "../../output/labelled/unlabelled.csv",
+            index=False,
+            sep=",",
+            encoding="utf-8",
+        )
+
+
+def count_pos():
+    labelled_df = pd.read_csv("../../output/labelled/labelled.csv")
+    print(labelled_df["Label"].sum())
+
+
+if __name__ == "__main__":
+    count_pos()
